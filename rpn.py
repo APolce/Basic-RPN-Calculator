7e126fcd3f4f183fbb39124edfc4d48f2489991f
Anthony Polce <appolce@umich.edu>
2018-10-26 15:50:23 -0400

    Passed first addition test

--- rpn.py
+++ rpn.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
+	# stack for calculator
+	stack =[]
+
+	# tokenize input
+	tokens = arg.split()	
+
+	# process tokens
+	for token in tokens:
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			val1 = stack.pop()
+			val2 = stack.pop()
+			result = val1 + val2
+			stack.append(result)
+			return stack[0]
 
 def main():
 	while True:
 		calculate(input("rpn calc> "))
 
-if __name__ == '__main__': # Note that's underscore unscore n a m e...
+if __name__ == '__main__': # Note that's underscore underscore n a m e...
 	main()
